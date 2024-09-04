ffb7c9aedfb7e2ccd272d1f065a06a24c131822a
Otávio Ferreira <ottvferreira@gmail.com>
2024-09-04 12:48:41 -0300

    FEAT: Cadastro de veiculos

---
+++ src/app/components/cadastro-motorista/cadastro-motorista.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-motorista',
+  standalone: true,
+  imports: [],
+  templateUrl: './cadastro-motorista.component.html',
+  styleUrl: './cadastro-motorista.component.scss'
+})
+export class CadastroMotoristaComponent {
+
+}
---
+++ src/app/pages/motorista/motorista.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-motorista',
+  standalone: true,
+  imports: [],
+  templateUrl: './motorista.component.html',
+  styleUrl: './motorista.component.scss'
+})
+export class MotoristaComponent {
+
+}
